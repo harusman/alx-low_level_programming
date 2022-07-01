dac7410aefb8076e242301e618f932913d9f4f47
harusman <westamder32@gmail.com>
2022-07-01 13:00:48 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Main - > assign a random number to the variable n each time it is executed
+ * and print the last digit of the number store
+ * Return : Always 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+putcha(ch);
+}
+putcha("\n");
+
+return(0);
+}
