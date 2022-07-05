d96853d2d4cb44ff3dcc2a8f3c376de8ecfc4213
harusman <westamder32@gmail.com>
2022-07-05 06:02:12 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,13 @@
-#include "holberton.h"
-/**
- * print_alphabet - print the alphabet in lowercase
- *
- * Return: void
- */
-void print_alphabet(void)
-{
-	int c = 'a';
+#include <unistd.h>
 
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
-	_putchar('\n');
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
