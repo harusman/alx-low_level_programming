83fff4e417f6620b8e15b65b62f70ae52c864f58
harusman <westamder32@gmail.com>
2022-07-22 10:31:36 -0700

    gcc

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,28 +1,28 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
- * _strncat - function that concatenates two strings
- *
- * @dest: parameter defined in main
- * @src: parameter defined in main
- * @n: parameter defined in main
- *
- * Return: memory address of function (string)
+/*
+ * File: 1-strncat.c
+ * Auth: HARUNA DANLADI MAINA
  */
 
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	char *tmp1 = dest;
-	char *tmp2 = src;
+	int index = 0, dest_len = 0;
 
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-	while (src < tmp2 + n && *src != '\0')
-	{
-		*dest = *src;
-		src++;
-		dest++;
+	while (dest[index++])
+		dest_len++;
 
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
