0e935a7a393e8c69d3367d7e24446161b3641a97
harusman <westamder32@gmail.com>
2022-07-25 08:14:03 -0700

    0x09. C

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "stddef.h"
+
+/**
+ * _memset - Fills memory with a constant byte.
+ * @s: source string.
+ * @b: the constant byte for filling.
+ * @n: lenght of buffer.
+ * Return: new string.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	
+	while (i < n)
+	{ 
+		*(s + i) = b;
+		i++;
+	}
+	
+	return (s);
+}
